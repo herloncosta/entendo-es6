3ca9cd37ae90238742a99a5716a128894387bb76
herloncosta <herlon36@gmail.com>
2022-04-18 21:12:03 -0300

    refactoring query-sql

--- src/functions/query-sql.js
+++ src/functions/query-sql.js
@@ -1,17 +1,14 @@
-function montaQuerySelect() {
-    const tabela = arguments[0];
-    const qtdArgs = arguments.length;
-    let cols = "";
-    if (qtdArgs > 1) {
-        for (let index = 1; index < qtdArgs; index++) {
-            cols += `${arguments[index]}, `; // `col1, `
-        }
-        cols = cols.substring(0, cols.length - 2); // `col1`
+const montaQuerySelect = (tabela, ...cols) => {
+    let colsQuery = "";
+    if (cols.length > 1) {
+        colsQuery = cols.reduce((query, col) => (query += `${col}, `), "");
+        colsQuery = colsQuery.slice(0, colsQuery.length - 2); // `col1`
     } else {
-        cols = "*";
+        colsQuery = "*";
     }
-    return `SELECT ${cols} from ${tabela}`; // SELECT * FROM tabela
-}
+    return `SELECT ${colsQuery} from ${tabela}`; // SELECT * FROM tabela
+};
+
 const query1 = montaQuerySelect("tabela");
 const query2 = montaQuerySelect("tabela", "col1");
 const query3 = montaQuerySelect("tabela", "col1", "col2");
