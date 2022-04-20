74086117133dc2a9713a8035464e70c25c535ca9
herloncosta <herlon36@gmail.com>
2022-04-19 22:13:03 -0300

    destructuring & labels updating

--- src/destructuring/labels.js
+++ src/destructuring/labels.js
@@ -8,7 +8,7 @@ const { sobrenome: apelido } = Pessoa;
 try {
     console.log(sobrenome);
 } catch (err) {
-    console.log(`${err.name}: ${err.message}`);
+    console.log(`${err.name}: ${err.message}`); // ReferenceError: sobrenome is not defined
 } finally {
-    console.log(apelido);
+    console.log(apelido); // Costa
 }
